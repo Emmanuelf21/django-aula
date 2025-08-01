8e8bec603c6b3f87f9373747012dde188d5bc539
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-07-31 21:44:31 -0300

    btn de adicionar imagem

--- recipes/models.py
+++ recipes/models.py
@@ -3,9 +3,15 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
+    cooking_time = models.IntegerField(help_text="Em minutos")
     ingredients = models.TextField()
     instructions = models.TextField()
-    cooking_time = models.IntegerField(help_text="Em minutos")
+    #faz o upload da imagem na MEDIA_ROOT/recipes
+    image = models.ImageField(upload_to='recipes/', blank=True, null=True)
     
     def __str__(self):
         return self.title
+    
+    class Meta:
+        # Define o nome da tabela no plural para o painel de administração
+        verbose_name_plural="Recipes"
